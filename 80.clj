ac3ad3172f82d6ccf9e026fdf46a8c8ce14ae974
David Young-chan Kay <davidykay@gmail.com>
2016-02-21 10:16:31 -0800

    concise solution for 80.

--- 80.clj
+++ 80.clj
@@ -1,13 +1,9 @@
 (ns user)
 
-(defn divisors [n]
-  (remove #(= n %)
-          (set (filter #(= 0 (mod n %)) (range 1 (inc n))))))
-
-(defn perfect?
-  "a number is 'perfect' if the sum of its divisors equal the number itself."
-  [n]
-  (= (apply + (divisors n))
+(defn perfect? [n]
+  (= (apply + 
+            (filter #(= 0 (mod n %))
+                    (range 1 n)))
      n))
 
 (= (perfect? 6) true)
