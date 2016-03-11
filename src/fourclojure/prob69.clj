b20e168cdc3d22daa9f12fca6aa6623f551e3fda
David Young-chan Kay <davidykay@gmail.com>
2016-03-10 16:38:12 -0800

    condensed 69

--- src/fourclojure/prob69.clj
+++ src/fourclojure/prob69.clj
@@ -1,11 +1,8 @@
 (ns fourclojure.prob69)
 
 (defn my-merge [f & maps]
-  (->> (for [k (set (flatten (map keys maps)))
-             m maps]
-         [k (get m k)])
-       (remove #(nil? (last %)))
-       (group-by first)
-       (map (fn [[k vs]]
-              [k (reduce f (map last vs))]))
+  (->> (apply concat maps)
+       (group-by key)
+       (map (fn [[k kvs]]
+              [k (reduce f (map val kvs))]))
        (into {})))
